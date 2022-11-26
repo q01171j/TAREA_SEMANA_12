4cc0f89bed581aeacf2b688d28cd6c4862b148ad
q01171j <q01171j@upla.edu.pe>
2022-11-26 09:20:08 -0500

    caguixtox

---
+++ src/main/java/com/mycompany/estructura_datos_sem12/ejercicio_03.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.estructura_datos_sem12;
+
+/**
+ *
+ * @author Adriano
+ */
+public class ejercicio_03 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
